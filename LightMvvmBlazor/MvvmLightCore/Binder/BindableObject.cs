d7c305dde9f9a6e8d95c5d5e74002ae82d2d7ba0
Devesh Bahuguna <bahuguna.devesh95@gmail.com>
2022-08-05 00:47:36 +0530

    Initial Project structure created

---
+++ LightMvvmBlazor/MvvmLightCore/Binder/BindableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLightCore.Binder
+{
+    internal class BindableObject
+    {
+        public INotifyPropertyChanged ViewModel { get; set; }
+        public PropertyInfo Property { get; set; }    
+    }
+}
---
+++ LightMvvmBlazor/MvvmLightCore/Binder/MvvmBinder.cs
@@ -0,0 +1,40 @@
+﻿using MvvmLightCore.Binder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLightCore
+{
+    public class MvvmBinder
+    {
+        public TValue Bind<TInput, TValue>( INotifyPropertyChanged viewmodel,Expression<Func<TInput, TValue>> bindingExpression) where TInput : INotifyPropertyChanged
+        {
+            BindableObject bindableObj = new BindableObject();
+            bindableObj.ViewModel = viewmodel;
+            bindableObj.Property = ParseBindingExpression(bindingExpression);
+            bindableObj.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            INotifyPropertyChanged bindableVM = bindableObj.ViewModel;
+            return (TValue)bindableObj.Property.GetValue(bindableObj.ViewModel, null) ;
+
+        }
+
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            
+        }
+
+        private PropertyInfo ParseBindingExpression<TInput, TValue>(Expression<Func<TInput, TValue>> bindingExpression) where TInput : INotifyPropertyChanged
+        {
+            if (bindingExpression.NodeType == ExpressionType.Lambda && bindingExpression.Body is MemberExpression)
+            {
+                return ((bindingExpression.Body as MemberExpression).Member) as PropertyInfo;
+            }
+            throw new NotSupportedException();
+        }
+    }
+}
