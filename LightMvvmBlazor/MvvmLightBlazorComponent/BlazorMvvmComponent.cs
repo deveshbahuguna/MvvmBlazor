46c85f727c3bddbbf781e1b72c161572ff0d7c2b
Devesh Bahuguna <bahuguna.devesh95@gmail.com>
2022-09-04 21:08:40 +0530

    Nuget references updated

--- LightMvvmBlazor/MvvmLightBlazorComponent/BlazorMvvmComponent.cs
+++ LightMvvmBlazor/MvvmLightBlazorComponent/BlazorMvvmComponent.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MvvmLightCore;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MvvmLightBlazorComponent
 {
